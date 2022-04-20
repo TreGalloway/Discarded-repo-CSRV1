85759c24c130570c387aa54e050375b436daefbb
Tre <tre@Tres-MacBook-Air.local>
2022-04-20 12:16:32 -0500

    finishing up some basic touches on site, now waiting for photos

---
+++ src/components/amenities/amenities.component.jsx
@@ -0,0 +1,22 @@
+const Amenities = () => {
+    return (
+        <div className="items-center card max-w bg-base-100">
+            <div className="items-center card-body">
+                <h2 className="text-3xl underline card-title decoration-primary">
+                    Amenities
+                </h2>
+                <p className="lg:px-20">
+                    Custom Deck/Picnic tables/BBQ Pit/Fire Pit/Sand Box/Great
+                    Landscaping
+                </p>
+                <p className="lg:px-20">
+                    Yamaha Golf Cart (*$75/day…Based on availability)
+                </p>
+
+                <div className="justify-end card-actions"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Amenities
