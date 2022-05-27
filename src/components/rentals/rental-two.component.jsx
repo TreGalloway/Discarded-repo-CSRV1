f67bfc788de8e4a2c6ad257cc567b7f58d4bd7b9
Tre <tre@Tres-MacBook-Air.local>
2022-05-27 15:07:18 -0500

    trouble shooting error: deleted package json lock

--- src/components/rentals/rental-two.component.jsx
+++ src/components/rentals/rental-two.component.jsx
@@ -1,84 +1,8 @@
 import { Link } from 'react-scroll/modules'
 
-import outside1 from '/Users/tre/coastalsoulrv/src/assets/rental2/outside 1.JPEG'
-import outside2 from '/Users/tre/coastalsoulrv/src/assets/rental2/outside 2.JPG'
-
-import inside1 from '/Users/tre/coastalsoulrv/src/assets/rental2/inside 1.JPEG'
-import inside2 from '/Users/tre/coastalsoulrv/src/assets/rental2/inside 2.JPEG'
-import inside3 from '/Users/tre/coastalsoulrv/src/assets/rental2/inside 3.JPEG'
-import inside4 from '/Users/tre/coastalsoulrv/src/assets/rental2/inside 4.JPEG'
-
 const Rental2 = () => {
     return (
         <div class="card w-full bg-base-100 shadow-xl justify-items-center">
-            <div className="w-full carousel">
-                <div id="slide1-b" className="relative w-full carousel-item">
-                    <img src={outside1} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide6-b" className="btn btn-circle">
-                            ❮
-                        </a>
-                        <a href="#slide2-b" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
-                </div>
-                <div id="slide2-b" className="relative w-full carousel-item">
-                    <img src={outside2} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1-b" className="btn btn-circle">
-                            ❮
-                        </a>
-                        <a href="#slide3-b" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
-                </div>
-                <div id="slide3-b" className="relative w-full carousel-item">
-                    <img src={inside1} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2-b" className="btn btn-circle">
-                            ❮
-                        </a>
-                        <a href="#slide4-b" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
-                </div>
-                <div id="slide4-b" className="relative w-full carousel-item">
-                    <img src={inside2} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3-b" className="btn btn-circle">
-                            ❮
-                        </a>
-                        <a href="#slide5-b" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
-                </div>
-                <div id="slide5-b" className="relative w-full carousel-item">
-                    <img src={inside3} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4-b" className="btn btn-circle">
-                            ❮
-                        </a>
-                        <a href="#slide6-b" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
-                </div>
-                <div id="slide6-b" className="relative w-full carousel-item">
-                    <img src={inside4} className="w-full" alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide5-b" className="btn btn-circle">
-                            ❮
-                        </a>
-                        <a href="#slide1-b" className="btn btn-circle">
-                            ❯
-                        </a>
-                    </div>
-                </div>
-            </div>
             <div class="card-body items-center lg:px-60">
                 <h2 class="card-title">Coastal Soul II (Lodging)</h2>
                 <p>
